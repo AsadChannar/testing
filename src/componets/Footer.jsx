255d6364f0bd50a088caf6de6b6350a82714e947
asad-fabtechsol <muhammad.asad@fabtechsol.com>
2025-03-13 11:03:01 +0500

    2nd commit

--- src/componets/Footer.jsx
+++ src/componets/Footer.jsx
@@ -7,39 +7,39 @@ import logo from '../images/tef-footer-logo.png'
 export default function Footer() {
   return (
     <>
-    <div className='footer-wrapper'>
-    <Container >
-      <Row className='info' >
-        <Col md={6} >
-        <img src={logo} />
+      <div className='footer-wrapper '>
+        <Container >
+          <Row className='info'>
+            <Col md={6} >
+              <img src={logo} />
 
-        <p>
-        - is an organization with a mandate to improve quality of education in rural areas of Southern Punjab.
-        </p>
-        </Col>
-        <Col md={3}  classNam="widgettitle">
-        <h3>Quick Links</h3>
-            <ul className="list-arrow">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Our Initiatives</a></li>
-              <li><a href="#">Social Gallery</a></li>
-              <li><a href="#">Downloads</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          
-        </Col>
-      <Col md={3} className="widget-title">
-      <h3 >Secondary Links</h3>
-            <ul className="list-arrow">
-              <li><a href="#">Site Map</a></li>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Login</a></li>
-            </ul>
-        </Col>
-      </Row>
-    </Container>
-    </div>
-     
+              <p>
+                - is an organization with a mandate to improve quality of education in rural areas of Southern Punjab.
+              </p>
+            </Col>
+            <Col md={3} classNam="widgettitle">
+              <h3>Quick Links</h3>
+              <ul className="list-arrow">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Initiatives</a></li>
+                <li><a href="#">Social Gallery</a></li>
+                <li><a href="#">Downloads</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+
+            </Col>
+            <Col md={3} className="widget-title">
+              <h3 >Secondary Links</h3>
+              <ul className="list-arrow">
+                <li><a href="#">Site Map</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Login</a></li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
 
     </>
   )
